ee13a082116906da2c10bc72ec05aef610f8b8a0
Yguanira <yguanirap@gmail.com>
2019-12-26 17:16:48 +0000

    Finalización estilo del modal de sign.

--- resources/views/partials/signModal.blade.php
+++ resources/views/partials/signModal.blade.php
@@ -35,24 +35,45 @@
                     @csrf
 
                     <!-- Sign In / Sign Up options -->
-                    <div class="btn-group btn-group-toggle" data-toggle="buttons" style="margin-bottom: 0.75%">
-                        <label class="btn btn-primary">
-                            <input type="radio" name="options" id="option1" autocomplete="off" value="0" checked> Público
-                        </label>
-                        <label class="btn btn-secondary">
-                            <input type="radio" name="options" id="option2" autocomplete="off" value="1"> Privado
-                        </label>
-                    </div>
+                        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-signform active">
+                                <input type="radio" name="options" id="option1" autocomplete="off" checked> INICIAR SESIÓN
+                            </label>
+                            <label class="btn btn-signform">
+                                <input type="radio" name="options" id="option2" autocomplete="off"> REGÍSTRATE
+                            </label>
+                        </div>
 
                     <br><br>
 
                     <!-- Email y contraseña -->
-                    <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" pattern="^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$" required>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña" minlength="8" maxlength="15" pattern="^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$+" required>
+                    <input type="email" class="form-control input-signform" id="email" name="email" placeholder="name@example.com" pattern="^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$" required>
+                    <input type="password" class="form-control input-signform" id="password" name="password" placeholder="Contraseña" minlength="8" maxlength="15" pattern="^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$+" required>
                     <br>
-                    <button type="submit">Enviar</button>
+                    <button class="signform-button" type="submit">Enviar</button>
 
                 </form>
+
+                <br>
+
+                <!-- Link para recuperar contraseña -->
+                <a class="forgotPassword" href="#">¿Has olvidado tu contraseña?</a>
+
+                <br><br>
+
+                <!-- Otros accesos rápidos -->
+                <div class="otherAccess">
+                    <p>Acceso rápido con:</p>
+
+                    <div class="social-navbar">
+                        <a class="google" href="#"><i class="fab fa-google"></i></a>
+                        <a class="facebook" href="#"><i class="fab fa-facebook-square"></i></a>
+                        <a class="instragram" href="#"><i class="fab fa-instagram"></i></a>
+                        <a class="twitter" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="pinterest" href="#"><i class="fab fa-pinterest"></i></a>
+                    </div>
+
+                </div>
 
             </div>
 
