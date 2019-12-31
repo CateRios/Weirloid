67f92fc66df36fac154388f773948cf96070b5a2
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2019-12-31 00:46:37 +0000

    Creacion de la tabla perfiles, ordenes y detalles

---
+++ database/migrations/2019_12_31_003511_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('txnid');
+            $table->integer('payment_amount');
+            $table->string('payment_status');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
